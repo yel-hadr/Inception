824d01af1b3eb41732ff03c8018d5cc914595cfd
yel-hadr <yel-hadr@students.1337.ma>
2024-04-24 16:53:51 +0100

    NO ERROR

--- Makefile
+++ Makefile
@@ -1,17 +1,16 @@
 all : up
 
 up : 
-	@sudo docker-compose -f ./srcs/docker-compose.yml up -d
+	@docker-compose -f ./srcs/docker-compose.yml up -d
 
 down : 
-	@sudo docker-compose -f ./srcs/docker-compose.yml down -v
-	@sudo docker system prune -af
+	@docker-compose -f ./srcs/docker-compose.yml down -v
 
 stop : 
-	@sudo docker-compose -f ./srcs/docker-compose.yml stop
+	@docker-compose -f ./srcs/docker-compose.yml stop
 
 start : 
-	@sudo docker-compose -f ./srcs/docker-compose.yml start
+	@docker-compose -f ./srcs/docker-compose.yml start
 
 status : 
-	@sudo docker ps
+	@docker ps
--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -1,9 +1,25 @@
-#!/bin/bash
+#/bin/sh
 
-# Start MariaDB
-service mysql start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
+
+# Adjust MariaDB configuration (optional)
+sed -i "s|bind-address           = 127.0.0.1|bind-address           = 0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+
+if [ ! -d "/run/mysqld" ]; then
+	mkdir /run/mysqld;
+fi
+	cat << EOF > /tmp/wp.sql
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};
+CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+EOF
+
+mariadbd --user=root --bootstrap < /tmp/wp.sql;
+rm -f /tmp/wp.sql;
+
+
+exec "$@"
